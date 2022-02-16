a7cb6d482d91a51664947189740ba3e4f4b22fa1
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-02-16 10:09:42 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -9,9 +9,31 @@
 #define Min_ChargeRate 0
 #define Max_ChargeRate 0.8
 
-char TemperatureWarning[] = "Temperature out of range!";
-char socWarning[] = "State of Charge out of range!";
-char chargeRateWarning[] = "Charge Rate out of range!" ;
+#define TEMP_WARNING_TOLERANCE  4
+#define SOC_WARNING_TOLERANCE   4
+#define CHARGERATE_WARNING_TOLERANCE  (float)0.2
+
+#define LANGUAGE ENGLISH
+
+#if(LANGUAGE == ENGLISH)
+const char *Notify[] ={"TEMPERATURE ALERT",
+                     "SOC ALERT",
+                     "CHARGE ALERT"
+                    };
+const char *Warning[] ={"TEMPERATURE OUT OF RANGE",
+                     "SOC OUT OF RANGE ",
+                     "CHARGE RATE TOO LOW"
+                    };
+#elif(LANGUAGE == GERMAN)
+const char *Notify[] ={"TEMPERATURALARM",
+                     "SOC-ALARM",
+                     "LADEZAHLALARM"
+                    };
+const char *Warning[] ={"TEMPERATUR AUSSERHALB DES BEREICHS",
+                     "SOC AUSSERHALB DER REICHWEITE",
+                     "LADESTUFE ZU NIEDRIG"
+                    };
+#endif
 
 void Display(char *DisplayText)
 {
@@ -19,42 +41,129 @@ void Display(char *DisplayText)
     printf("%s \n", DisplayText);
 }
 
-bool CheckValueOutOfRange (float input, float MinThreshold, float MaxThreshold)
+bool TemperatureAlarmCheck(float temperature)
 {
-  return (input < MinThreshold || input > MaxThreshold);
+	if((Min_Temperature + TEMP_WARNING_TOLERANCE ) > temperature || (Max_Temperature - TEMP_WARNING_TOLERANCE ) < temperature)
+	{
+		Display (Warning[0]);
+		return true;
+	}
+	else
+	{
+		return false;
+	}	
 }
 
-bool TemperatureRangeCheck (float temperature)
+bool TemperatureWarningCheck(float temperature)
 {
-  bool TemperaturStatus = CheckValueOutOfRange (temperature, (float) Min_Temperature, (float) Max_Temperature);
-  return TemperaturStatus;
+	if((Min_Temperature > temperature) || (Max_Temperature < temperature))
+	{
+		Display (Notify[0]);
+		return true;
+	}
+	else
+	{
+		return false;
+	}	
 }
 
-bool socRangeCheck (float soc)
+bool socAlarmCheck(float soc)
 {
-  bool socStatus = CheckValueOutOfRange (soc, (float) Min_soc, (float) Max_soc);
-  return socStatus;
+	if((Min_soc + SOC_WARNING_TOLERANCE ) > soc || (Max_soc  - SOC_WARNING_TOLERANCE ) < soc)
+	{
+		Display (Warning[1]);
+		return true;
+	}
+	else
+	{
+		return false;
+	}	
 }
 
-bool chargeRateCheck (float chargeRate)
+bool socWarningCheck(float soc)
 {
-  bool chargeRateStatus = CheckValueOutOfRange (chargeRate, (float) Min_ChargeRate, (float) Max_ChargeRate);
-  return chargeRateStatus;
+	if((Min_soc  > soc) || (Max_soc < soc))
+	{
+		Display (Notify[1]);
+		return true;
+	}
+	else
+	{
+		return false;
+	}	
 }
 
-bool BatteryValueIsNOk (float value, bool (*RangeCheck) (float), char *warning)
+bool chargeRateAlarmCheck(float chargerate)
 {
-  bool val = RangeCheck(value);
-  if (!val)
-     Display(warning);
-  return val;
+	if((Min_ChargeRate + CHARGERATE_WARNING_TOLERANCE ) > temperature || (Max_ChargeRate  - CHARGERATE_WARNING_TOLERANCE ) < temperature)
+	{
+		Display (Warning[2]);
+		return true;
+	}
+	else
+	{
+		return false;
+	}	
 }
-                         
+
+bool chargeRateWarningCheck(float chargerate)
+{
+	if((Min_ChargeRate > chargerate) || (Max_ChargeRate < chargerate))
+	{
+		Display (Notify[2]);
+		return true;
+	}
+	else
+	{
+		return false;
+	}	
+}
+bool TemperatureCheck (float temperature, bool (*PrintTemperatureWarning)(float), bool (*PrintTemperatureAlarm)(float))
+{
+  if (PrintTemperatureWarning (temperature))
+	{
+		return true;
+	}
+   else
+	{
+		return (PrintTemperatureAlarm(temperature));
+	}
+
+}
+
+bool socCheck (float soc, bool (*PrintSocWarning)(float), bool (*PrintSocAlarm)(float))
+{
+  if (PrintSocWarning (soc))
+	{
+		return true;
+	}
+   else
+	{
+		return (PrintSocAlarm(soc));
+	}
+
+}
+
+bool chargeRateCheck (float chargeRate, bool (*PrintChargeRateWarning)(float), bool (*PrintChargeRateAlarm)(float))
+{
+  if (PrintChargeRateWarning (soc))
+	{
+		return true;
+	}
+   else
+	{
+		return (PrintChargeRateAlarm(soc));
+	}
+
+}
+
+bool 
+                    
 bool BatteryIsNOk (float temperature, float soc, float ChargeRate)
 {
-  bool val = BatteryValueIsNOk(temperature, TemperatureRangeCheck, TemperatureWarning);
-  val = val || BatteryValueIsNOk(soc, socRangeCheck, socWarning);
-  val = val || BatteryValueIsNOk(ChargeRate, chargeRateCheck, chargeRateWarning);
+  bool val = TemperatureCheck (temperature, TemperatureWarningCheck, TemperatureAlarmCheck);
+  val = val || socCheck (soc, socWarningCheck, socAlarmCheck);
+  val = val || chargeRateCheck (ChargeRate, chargeRateWarningCheck, chargeRateAlarmCheck);
 }
 
 void TestCheckBatteryIsNOk (bool expectedstatus, float Temperature, float soc, float ChargeRate)
@@ -63,17 +172,9 @@ void TestCheckBatteryIsNOk (bool expectedstatus, float Temperature, float soc, f
   assert(teststatus == expectedstatus);
 }
 
-void TestCheckValueOutOfRange (bool expectedstatus, float testInput, float testMinThreshold, float testMaxThreshold)
-{
-  bool teststatus = CheckValueOutOfRange(testInput, testMinThreshold, testMaxThreshold);
-  assert(teststatus == expectedstatus);
-}
-
 int main() 
 {
-  TestCheckValueOutOfRange(1, 25, 20, 22);
-  TestCheckValueOutOfRange(0, 21, 20, 22);
-  
+ 
   TestCheckBatteryIsNOk(0, 25, 70, 0.7);
   TestCheckBatteryIsNOk(1, 50, 85, 0);
   
